4566d058d13187f5271cf51503bfe0a7a008c92b
Ismael Melgarejo <ismaelmelgarejo@gmail.com>
2024-10-24 14:53:16 -0300

    Pequeños cambios

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,18 @@ const blogSchema = z.object({
     }).optional(),
 });
 
+const projectsSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+        message: 'tags must be unique',
+    }).optional(),
+});
+
 const storeSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -25,12 +37,15 @@ const storeSchema = z.object({
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
+export type ProjectsSchema = z.infer<typeof projectsSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
+const projectsCollection = defineCollection({ schema: projectsSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 
 export const collections = {
     'blog': blogCollection,
+    'projects': projectsCollection,
     'store': storeCollection
 }
