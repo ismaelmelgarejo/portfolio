46cfab7087d70e45d903337e4c028e2edfa5d1b2
Ismael Melgarejo <ismaelmelgarejo@gmail.com>
2024-10-29 08:51:16 -0300

    Se modifico el blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blogSchema = z.object({
     tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
         message: 'tags must be unique',
     }).optional(),
+    link: z.string().url().optional(), //campo link
 });
 
 const storeSchema = z.object({
