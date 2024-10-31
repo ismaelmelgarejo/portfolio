5373be656a078f1d169727198e3f8fc9bf3a84ae
Ismael Melgarejo <ismaelmelgarejo@gmail.com>
2024-10-31 10:04:43 -0300

    Se modifico la descipcion de los puestos

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ismael Melgarejo | Personal Portfolio Website Template';
+export const SITE_TITLE = 'Ismael Melgarejo | Personal Portfolio';
 export const SITE_DESCRIPTION = 'A simple blog built with Astro, Astrofy is a free and open-source template for creating a personal portfolio website, developed with Astro and TailwindCSS.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
