a87bcadb905fc3617c5bf40f6c777f9f227f3971
ISmael Melgarejo <ismaelmelgarejo@gmail.com>
2026-03-11 11:08:43 -0300

    new portfolio

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Ismael Melgarejo | Personal Portfolio';
-export const SITE_DESCRIPTION = 'A basic blog built with Astro, utilizing the Astrofy template.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
